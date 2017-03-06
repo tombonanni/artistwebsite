51e354d6f8d0060b09596de78868e5289bc61c60
tombonanni <tombonanni@gmail.com>
2017-03-06 12:03:27 -0500

    added carousel at homepage

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,29 @@
-/**
- * GET /
- * Home page.
- */
+const fs = require('fs');
+const User = require('../models/User');
+
 exports.index = (req, res) => {
-  res.render('home', {
-    title: 'Home'
+  const uploadFolder = 'public/gallery';
+  var images = [];
+  fs.readdir(uploadFolder, (err, files) => {
+    files.forEach(file => {
+      images.push(file)
+    });
+  });
+  User.findOne({ email: "joe@admin.com" }, function(err, user){
+    if (err) console.log(err);
+    if (user === null) {
+      res.render('home', {
+        title: 'Home',
+        images: images
+      });
+    } else {
+      res.render('home', {
+        title: 'Home',
+        images: images,
+        twitter: user.twitter,
+        facebook: user.facebook,
+        instagram: user.instagram
+      });
+    }
   });
 };
